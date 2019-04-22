6e4e31a8dc3af7137e390a11676f0c2e0738778f
Rob <dontbugme@robinomicon.com>
2019-04-21 23:33:16 -0400

    Update System.php

--- handlers/System.php
+++ handlers/System.php
@@ -1,22 +1,5 @@
 <?php
 
-/*
-
-{
-    "mini.injector":[
-        {"version": "3.0.19rc4", "changes":[
-            "Added changelog to API, laid groundwork for injector",
-            "Something else here...",
-        ]},
-        {"version": "3.0.19rc3", "changes":[
-
-        ]}
-    ],
-}
-
-
-*/
-
 namespace handlers {
     class System {
 
@@ -25,7 +8,7 @@ namespace handlers {
                 if ($input['key'] == \DEPLOY_KEY) {
                     $reset = explode("\n", shell_exec("cd ".rtrim(\PATH_ROOT, "/")." && git reset --hard 2>&1"));
                     $pull = explode("\n", shell_exec("cd ".rtrim(\PATH_ROOT, "/")." && git pull 2>&1"));
-                    return array_filter(array_merge($reset,$pull));
+                    return array_values(array_filter(array_merge($reset,$pull)));
                 } else {
                     throw new \errors\NotAuthorized();
                 }
