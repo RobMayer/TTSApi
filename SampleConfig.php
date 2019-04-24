d7f78bf99f778b291c61b6166443a1b1d867cff5
Rob <dontbugme@robinomicon.com>
2019-04-23 22:13:18 -0400

    Create SampleConfig.php

---
+++ SampleConfig.php
@@ -0,0 +1,19 @@
+<?php
+
+define('SALT', "ThisIsNotMySalt");
+define('DEPLOY_KEY', "12345");
+
+define('PATH_ROOT', "C:/workspace/web_lib/tts/");
+define('URL_ROOT', "http://localhost/tts/");
+
+define('PATH_LOGS', PATH_ROOT."logs/");
+
+define('SYS_MODE', "DEV");
+define('SYS_SEND_LEVEL', SYS_LOG_ALL);
+define('SYS_MASK_LEVEL', SYS_LOG_FTL);
+define('SYS_LOG_LEVEL', SYS_LOG_ALL);
+define('SYS_LOG_PREFIX', "TTS_");
+
+define('SYS_VERSION', '0.1.0');
+
+?>
